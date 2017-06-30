19427a4453383be06bd2175100500eb7c4bffe3e
9898287 <mail.adnan.05@gmail.com>
2017-07-01 01:17:01 +1000

    Create main.d

---
+++ main.d
@@ -0,0 +1,20 @@
+void main()
+{
+    import linq5;
+    linq5.perform();
+
+    import linq9;
+    linq9.perform();
+
+    import linq14;
+    linq14.perform();
+
+    import linq25;
+    linq25.perform;
+
+    import linq27;
+    linq27.perform();
+
+    import linq31;
+    linq31.perform();
+}
