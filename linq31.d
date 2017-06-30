a75e269ce367564aec62de516f404543b29dcba2
9898287 <mail.adnan.05@gmail.com>
2017-07-01 01:39:21 +1000

    Update linq31.d

--- linq31.d
+++ linq31.d
@@ -5,5 +5,5 @@ import std.algorithm, std.stdio, std.uni, std.range;
 void perform()
 {
     immutable words = ["aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry"];
-    words.dup.map!(toLower).writeln;
+    //TODO
 }
